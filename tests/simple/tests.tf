417a0f58e1e65101c17540e1628fc48a4d847c51
Nic <nic.ceynowa@gmail.com>
2021-02-21 14:36:05 -0500

    simple tests added

---
+++ tests/simple/tests.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  profile = "test"
+  region  = "us-east-1"
+  alias   = "test"
+}
+
+module "under_test" {
+  source = "../.."
+  profile = "test"
+  region = "us-east-1"
+  base_cidr = "10.0.0.0/16"
+
+  providers = {
+    aws = aws.test
+  }
+}
